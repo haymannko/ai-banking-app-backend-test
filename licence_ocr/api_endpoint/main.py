eb1f2fcf70af9fba7d0b84f0310fbe1ff8c2dab1
Ye Bhone Lin <yebhonelin@Yes-MacBook-Air.local>
2025-09-20 18:09:19 +0630

    monitoring app

--- licence_ocr/api_endpoint/main.py
+++ licence_ocr/api_endpoint/main.py
@@ -5,9 +5,13 @@ from contextlib import asynccontextmanager
 import os 
 import shutil
 from typing import Literal
+import sentry_sdk
+from sentry_sdk.integrations.starlette import StarletteIntegration
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+import uuid
+from dotenv import load_dotenv
 
-from prometheus_fastapi_instrumentator import Instrumentator
-
+load_dotenv()
 
 ocr_model = {}
 
@@ -21,34 +25,50 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DSN"),
+    integrations=[
+        StarletteIntegration(transaction_style="endpoint"),
+        FastApiIntegration(transaction_style="endpoint"),
+    ],
+    traces_sample_rate=1.0,  
+    send_default_pii=True,
+)
+
 @app.post("/ocr")
 def ocr_endpoint(file: UploadFile = File(...), class_name: Literal['passport', 'licence'] = Form(...)
 ):
     """Endpoint to handle OCR requests."""
     try:
-        temp_file = f"temp_{file.filename}"
+        temp_file = f"temp_{uuid.uuid4()}.jpg"
         with open(temp_file, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-        
+
         ocr = ocr_model["OCR_Model"]
         ocr.image_path = temp_file
 
-        if class_name == "passport":
-            gray = ocr.preprocess_image_for_passport_ocr()
-            result = ocr.passport_ocr_model(gray)
+        with sentry_sdk.start_transaction(op="task", name=f"OCR-{class_name}"):
+            if class_name == "passport":
+                with sentry_sdk.start_span(op="preprocess", description="Preprocess Passport"):
+                    gray = ocr.preprocess_image_for_passport_ocr()
 
-        elif class_name == "licence":
-            gray = ocr.preprocess_image_for_licence_ocr()
-            result = ocr.licence_ocr_model(gray)
+                with sentry_sdk.start_span(op="model", description="Passport OCR Model"):
+                    result = ocr.passport_ocr_model(gray)
+                    os.remove(temp_file) 
 
-        os.remove(temp_file)
+            elif class_name == "licence":
+                with sentry_sdk.start_span(op="preprocess", description="Preprocess Licence"):
+                    gray = ocr.preprocess_image_for_licence_ocr()
+
+                with sentry_sdk.start_span(op="model", description="Licence OCR Model"):
+                    result = ocr.licence_ocr_model(gray)
+                    os.remove(temp_file) 
 
         return {"data": result}
-        
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
-    
 
-@app.lifespan("startup")
-async def startup():
-    Instrumentator().instrument(app).expose(app)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)  # send detailed error to Sentry
+        raise HTTPException(status_code=500, detail="OCR processing failed")
+    
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=5000)
